2f68659701ea692d14153a81066c661ff48db8f6
Bruno Gianfranco Ruiz Ramirez <b.ruiz@pucp.edu.pe>
2019-04-22 10:54:55 -0500

    Sin flask_excel

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from app import app
-import flask_excel as excel
+#import flask_excel as excel
 
 if __name__ == "__main__":
-  excel.init_excel(app)
+  #excel.init_excel(app)
   app.run()
